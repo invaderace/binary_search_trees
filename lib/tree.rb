32f3a77956f12fec01fdcdd752725d45e31c8746
Michael Mattson <michael.p.mattson@gmail.com>
2021-02-13 15:42:19 -0700

    added insert method

--- lib/tree.rb
+++ lib/tree.rb
@@ -7,7 +7,7 @@ class Tree
   attr_accessor :root
 
   def initialize(array)
-    @root = build_tree(array, first = 0, last = array.length - 1 )
+    @root = build_tree(array.sort.uniq, first = 0, last = array.length - 1 )
   end
 
   # initialize start = 0, end = length of the array - 1
@@ -17,19 +17,24 @@ class Tree
   # calculate mid of left subarray and make it root of left subtree of A.
   # calculate mid of right subarray and make it root of right subtree of A.
   def build_tree(array, first, last)
-    return nil if first > last
+    return nil if first > last || array[first].nil?
 
     mid = (first + last) / 2
     root_node = Node.new(array[mid])
-
     root_node.left = build_tree(array, first, mid - 1)
     root_node.right = build_tree(array, mid + 1, last)
-    # left = array[0..mid - 1])
-    # right = array[mid + 1..last]
     root_node
   end
 
-  def insert(value)
+  def insert(value, comparing = @root)
+    return @root = Node.new(value) if comparing.nil?
+    return nil if value == comparing
+
+    if value < comparing.value
+      comparing.left.nil? ? comparing.left = Node.new(value) : insert(value, comparing.left)
+    elsif value > comparing.value
+      comparing.right.nil? ? comparing.right = Node.new(value) : insert(value, comparing.right)
+    end
   end
 
   def delete(value)
