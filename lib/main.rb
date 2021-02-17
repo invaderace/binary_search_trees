7f899481acafe9d7ebe533f13fc2b70833e1c219
Michael Mattson <michael.p.mattson@gmail.com>
2021-02-17 08:43:09 -0700

    added driver script

--- lib/main.rb
+++ lib/main.rb
@@ -1,17 +1,34 @@
-# get the middle element and make it root.
-# recursively contruct the left subtree and make it the left subchild of the root.
-# recursively contruct the right subtree and make it the right subchild of the root.
-
-
-
-# initialize start = 0, end = length of the array - 1
-# mid = (start+end)/2
-# create a tree node with mid as root (lets call it A)
-# recursively do the following steps:
-# calculate mid of left subarray and make it root of left subtree of A.
-# calculate mid of right subarray and make it root of right subtree of A.
-
 require_relative 'tree.rb'
 
-tree = Tree.new([1,2,3,4,5,6,7,8,9])
-p tree
+tree = Tree.new( Array.new(15) { rand(100) } )
+tree.pretty_print
+puts
+
+puts "Tree is balanced? #{tree.balanced?}"
+puts
+
+puts "Level-order: #{tree.level_order}"
+puts "Pre-order: #{tree.preorder}"
+puts "Post-order: #{tree.postorder}"
+puts "In-order: #{tree.inorder}"
+puts
+
+5.times { tree.insert(rand(101..150)) }
+tree.pretty_print
+puts
+
+puts "Tree is balanced? #{tree.balanced?}"
+puts
+
+tree.rebalance
+tree.pretty_print
+puts
+
+puts "Tree is balanced? #{tree.balanced?}"
+puts
+
+puts "Level-order: #{tree.level_order}"
+puts "Pre-order: #{tree.preorder}"
+puts "Post-order: #{tree.postorder}"
+puts "In-order: #{tree.inorder}"
+puts
